8c5a54057d3d914fb0a85eee7d1c130cb536635b
25LP7053 25LP7053 <johndoe@example.com>
2026-03-06 11:53:41 +0800

    aa

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   env: {
     PRISMA_DATABASE_URL: process.env.PRISMA_DATABASE_URL,
+    CLERK_WEBHOOK_SIGNING_SECRET: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
   },
 };
 
